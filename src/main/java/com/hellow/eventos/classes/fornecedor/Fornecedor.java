1c8e26443658326e9d960c3a3bc8b297a9a843cb
ALICIA CRISTINA DA CUNHA <alicia_cunha@estudante.sesisenai.org.br>
2024-08-15 10:32:49 -0300

    Controllers e Repositories

---
+++ src/main/java/com/hellow/eventos/classes/fornecedor/Fornecedor.java
@@ -0,0 +1,28 @@
+package com.hellow.eventos.classes.fornecedor;
+
+import com.hellow.eventos.classes.tipo.Tipo;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name="fornecedor")
+@Table(name="fornecedor")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Fornecedor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column Integer id_fornecedor;
+
+    private String nome;
+    private Integer cnpj;
+    private Boolean ativo;
+    @ManyToOne
+    @JoinColumn(name="tipo_id")
+    private Tipo tipo;
+
+}
